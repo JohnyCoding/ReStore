8d8240e6a7bd06fe98891c048352621ebb897de3
JohnyCoding <vanja.kocubovski@gmail.com>
2024-03-24 23:51:12 +0100

    docker integration done

---
+++ client/src/app/layout/SignedInMenu.tsx
@@ -0,0 +1,48 @@
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/configureStore";
+import { signOut } from "../../features/account/accountSlice";
+import { clearBasket } from "../../features/basket/basketSlice";
+import { Link } from "react-router-dom";
+
+export default function SignedInMenu() {
+	const dispatch = useAppDispatch();
+	const { user } = useAppSelector((state) => state.account);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: any) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<>
+			<Button color="inherit" sx={{ typography: "h6" }} onClick={handleClick}>
+				{user?.email}
+			</Button>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				MenuListProps={{
+					"aria-labelledby": "basic-button",
+				}}
+			>
+				<MenuItem onClick={handleClose}>Profile</MenuItem>
+				<MenuItem component={Link} to="/orders">
+					My orders
+				</MenuItem>
+				<MenuItem
+					onClick={() => {
+						dispatch(signOut());
+						dispatch(clearBasket());
+					}}
+				>
+					Logout
+				</MenuItem>
+			</Menu>
+		</>
+	);
+}
